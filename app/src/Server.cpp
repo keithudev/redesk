b1372a738d97f07b4171722bb50530f8bf3f181b
keithudev <159966983+keithudev@users.noreply.github.com>
2025-02-19 22:01:36 +0100

    Added FFmpeg and stuff

---
+++ app/src/Server.cpp
@@ -0,0 +1,7 @@
+#include "server.h"
+#include <SDL3/SDL.h>
+
+void startServer() {
+    SDL_Log("Starting server...");
+    SDL_Quit();
+}
--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,6 +1,18 @@
+#include <iostream>
+#include <string>
 #include <SDL3/SDL.h>
+#include "server.h"
 
-int main() {
-    SDL_Log("%s", "Redesk CLI");
+int main(int argc, char* argv[]) {
+    if(argc < 2) {
+        printf("usage: %s [options]", argv[0]);
+        return 0;
+    }
+    std::string option = argv[1];
+
+    if (option == "--stream") {
+        startServer();
+    }
+
     return 0;
 }
